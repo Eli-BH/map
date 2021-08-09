cd99464e6c57c662a2d24957fa7f2d659b15a982
Elijah <Eli.i.henderson95@gmail.com>
2021-08-09 10:55:18 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+import List from './components/List/List';
 
-function App() {
+import { getPlacesData } from './api';
+
+import { CssBaseline, Grid } from '@material-ui/core';
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({
+    lat: 40.76073742039887,
+    lng: -73.98276131657323,
+  });
+  const [bounds, setBounds] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //get users lat and lng
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, [coordinates, bounds]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    bounds &&
+      getPlacesData(bounds.sw, bounds.ne)
+        .then((data) => {
+          setPlaces(data);
+          setIsLoading(false);
+        })
+        .catch((err) => console.error(err));
+  }, [bounds, coordinates]);
+  console.log(bounds);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
